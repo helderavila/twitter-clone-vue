75bb590482e790203717a5428a0674913259455d
helderavila <helderavila2016@gmail.com>
2020-01-19 12:40:57 -0300

    fake api created

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const vm = new Vue({
   el: "#app",
   data: {
-
+    tweets: [],
+    teste: 'Olá'
+  },
+  methods: {
+    fetchTweets() {
+      fetch('./api/tweets.json')
+        .then(r => r.json())
+        .then(r => {
+          this.tweets = r;
+        })
+    }
+  },
+  created() {
+    this.fetchTweets()
   }
 })
